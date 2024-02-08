14e52fe6aa8efc94bd15ce74da15b93571b4fb8a
LucasPerrotaRoriz <Lucaswebdeveloper@proton.me>
2024-02-07 21:43:08 -0300

    JS for random selection is done

--- 13 - Random Choice Picker/script.js
+++ 13 - Random Choice Picker/script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -13,7 +21,7 @@ function createTags(input) {
     .filter((tags) => tags.trim() !== "")
     .map((tag) => tag.trim());
 
-    tagsEl.innerHTML = ''
+  tagsEl.innerHTML = "";
 
   tags.forEach((tag) => {
     const tagEl = document.createElement("span");
@@ -21,4 +29,42 @@ function createTags(input) {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    if(randomTag !== undefined) {
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100);
+    }
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
