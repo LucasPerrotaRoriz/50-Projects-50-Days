bbfa32a438802a21712319b9be597358b7e0ad93
LucasPerrotaRoriz <Lucaswebdeveloper@proton.me>
2024-02-22 11:41:05 -0300

    Dynamic profile cards and adding repos to card JS

--- 28 - Github Profiles/script.js
+++ 28 - Github Profiles/script.js
@@ -13,12 +13,29 @@ async function getUser(username) {
     const { data } = await axios(APIURL + username);
     // console.log(data);
 
-   
+    createUserCard(data);
+    getRepos(username);
   } catch (err) {
     if (err.response.status == 404) {
-      
+      createErrorCard("No profile with this username");
     }
   }
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById("repos");
+
+  repos
+    .slice(0, 5)
+    .forEach((repo) => {
+        const repoEl = document.createElement("a");
+        repoEl.classList.add("repo");
+        repoEl.href = repo.html_url;
+        repoEl.target = "_blank";
+        repoEl.innerText = repo.name;
+
+    reposEl.appendChild(repoEl);
+  });
 }
 
 form.addEventListener("submit", (e) => {
@@ -32,3 +49,45 @@ form.addEventListener("submit", (e) => {
     search.value = "";
   }
 });
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + "/repos?sort=created");
+    addReposToCard(data);
+  } catch (err) {
+    createErrorCard("Problem fetching repo");
+  }
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+    <div class="card">
+        <div>
+            <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+        </div>
+        <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+            <li>${user.followers} <strong>Followers</strong></li>
+            <li>${user.following} <strong>Following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos"></div>
+        </div>
+    </div>
+    `;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `;
+
+  main.innerHTML = cardHTML;
+}
