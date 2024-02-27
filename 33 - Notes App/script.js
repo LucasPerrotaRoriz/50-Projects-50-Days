d98d0adc59df366d1f33bb563a693b96c94a403b
LucasPerrotaRoriz <Lucaswebdeveloper@proton.me>
2024-02-27 12:08:59 -0300

    JS Save notes to local storage

--- 33 - Notes App/script.js
+++ 33 - Notes App/script.js
@@ -1,5 +1,11 @@
 const addBtn = document.getElementById("add");
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
 addBtn.addEventListener("click", () => addNewNote(""));
 
 function addNewNote(text = "") {
@@ -30,11 +36,15 @@ function addNewNote(text = "") {
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
+
+    updateLocalStorage();
   });
 
   editBtn.addEventListener("click", () => {
     main.classList.toggle("hidden");
     textArea.classList.toggle("hidden");
+
+    updateLocalStorage();
   });
 
   textArea.addEventListener('input', (e) => {
@@ -43,4 +53,12 @@ function addNewNote(text = "") {
   })
 
   document.body.appendChild(note);
+}
+
+function updateLocalStorage() {
+    const notesText = document.querySelectorAll('textarea');
+    const notes = [];
+    notesText.forEach(note => notes.push(note.value));
+   
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
